43b99c9ea8fd2b946823673ad4e5559779766392
atsushi0919 <hatake0919@yahoo.co.jp>
2022-04-04 16:36:34 +0900

    セクション3 投稿用の編集

--- 03-04_split_input_step4.rb
+++ 03-04_split_input_step4.rb
@@ -1,6 +1,55 @@
 # 半角スペース区切りの 10 個の入力 (paizaランク D 相当)
 # https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step4
 
+# 入出力例
+INPUT1 = <<~"EOS"
+  VypymzW25l 3xsXWAK NYwbs2MRC kh4 mr38o8W9 94 fEJ 6THlW fJFTy n
+EOS
+OUTPUT1 = <<~"EOS"
+  VypymzW25l
+  3xsXWAK
+  NYwbs2MRC
+  kh4
+  mr38o8W9
+  94
+  fEJ
+  6THlW
+  fJFTy
+  n
+EOS
+
+INPUT2 = <<~"EOS"
+  VDHUoNUym oqnbK7XsHK GPuafB yWdzgE bUMaMdBV 3kDU9 AbWnot 1E44Ekwz jdu Y0e
+EOS
+OUTPUT2 = <<~"EOS"
+  VDHUoNUym
+  oqnbK7XsHK
+  GPuafB
+  yWdzgE
+  bUMaMdBV
+  3kDU9
+  AbWnot
+  1E44Ekwz
+  jdu
+  Y0e
+EOS
+
+INPUT3 = <<~"EOS"
+  F5G6 0KSYzlF ghYYhAw NHCSxgPMbE EYqKRElk 8UYzUH 6mxtW 0tt1n 2cEaAQs gAWn
+EOS
+OUTPUT3 = <<~"EOS"
+  F5G6
+  0KSYzlF
+  ghYYhAw
+  NHCSxgPMbE
+  EYqKRElk
+  8UYzUH
+  6mxtW
+  0tt1n
+  2cEaAQs
+  gAWn
+EOS
+
 # 解答例1
 # 入力
 ary = gets.chomp.split(" ")
@@ -9,89 +58,15 @@ puts(ary)
 
 # 解答例2
 # 入力
-ary = gets.chomp.split(" ")
+s1, s2, s3, s4, s5, s6, s7, s8, s9, s10 = gets.chomp.split(" ")
 # 出力
-ary.each { |s| puts(s) }
-
-=begin
-半角スペース区切りの 10 個の入力 (paizaランク D 相当)
-問題にチャレンジして、ユーザー同士で解答を教え合ったり、コードを公開してみよう！
-
-シェア用URL:
-https://paiza.jp/works/mondai/stdin_primer/stdin_primer__split_input_step4
-問題文のURLをコピーする
- 下記の問題をプログラミングしてみよう！
-文字列 s_1, s_2, ... s_9, s_10 が半角スペースで区切られて 1 行で与えられます。
-各文字列を出力するごとに改行し 10 行で出力してください。
-
-▼　下記解答欄にコードを記入してみよう
-
-入力される値
-以下の形式で標準入力によって与えられます。
-文字列 s_1, s_2, ... s_9, s_10 が半角スペースで区切られて 1 行で与えられます。
-
-
-s_1 s_2 ... s_9 s_10
-
-入力値最終行の末尾に改行が１つ入ります。
-文字列は標準入力から渡されます。 標準入力からの値取得方法はこちらをご確認ください
-期待する出力
-入力された文字列 s_1, s_2, ... s_9, s_10 をそれぞれ改行区切りで出力してください。
-また、末尾に改行を入れ、余計な文字、空行を含んではいけません。
-
-
-s_1
-s_2
-...
-s_9
-s_10
-条件
-すべてのテストケースにおいて、以下の条件をみたします。
-
-・s_1, s_2, ... s_9, s_10 は数字または大文字・小文字のアルファベットからなる長さ 1 以上 10 以下の文字列
-
-入力例1
-VypymzW25l 3xsXWAK NYwbs2MRC kh4 mr38o8W9 94 fEJ 6THlW fJFTy n
-
-出力例1
-VypymzW25l
-3xsXWAK
-NYwbs2MRC
-kh4
-mr38o8W9
-94
-fEJ
-6THlW
-fJFTy
-n
-
-入力例2
-VDHUoNUym oqnbK7XsHK GPuafB yWdzgE bUMaMdBV 3kDU9 AbWnot 1E44Ekwz jdu Y0e
-
-出力例2
-VDHUoNUym
-oqnbK7XsHK
-GPuafB
-yWdzgE
-bUMaMdBV
-3kDU9
-AbWnot
-1E44Ekwz
-jdu
-Y0e
-
-入力例3
-F5G6 0KSYzlF ghYYhAw NHCSxgPMbE EYqKRElk 8UYzUH 6mxtW 0tt1n 2cEaAQs gAWn
-
-出力例3
-F5G6
-0KSYzlF
-ghYYhAw
-NHCSxgPMbE
-EYqKRElk
-8UYzUH
-6mxtW
-0tt1n
-2cEaAQs
-gAWn
-=end
+puts(s1)
+puts(s2)
+puts(s3)
+puts(s4)
+puts(s5)
+puts(s6)
+puts(s7)
+puts(s8)
+puts(s9)
+puts(s10)
