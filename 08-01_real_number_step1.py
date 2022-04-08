3560b396ac657fa3d982dad6152d25f6336af3d2
atsushi0919 <hatake0919@yahoo.co.jp>
2022-04-08 12:01:42 +0900

    セクション8 投稿用の編集

--- 08-01_real_number_step1.py
+++ 08-01_real_number_step1.py
@@ -1,27 +1,10 @@
 # 改行区切りでの N 個の実数の入力 (paizaランク D 相当)
 # https://paiza.jp/works/mondai/stdin_primer/stdin_primer__real_number_step1
 
-# 入出力例
-INPUT1 = <<~"EOS"
-  5
-  0.813
-  8.13
-  81.3
-  813
-  8138
-EOS
-OUTPUT1 = <<~"EOS"
-  0.813
-  8.13
-  81.3
-  813
-  8138
-EOS
-
 # 解答例
 # 1 行目入力
-n = gets.to_i
+n = int(input())
 # 続く n 行入力
-ary = n.times.map { gets.to_f }
+ary = [float(input()) for _ in range(n)]
 # 出力
-puts(ary)
+[print(f) for f in ary]
--- 08-02_real_number_step2.py
+++ 08-02_real_number_step2.py
@@ -1,23 +1,10 @@
 # 2 行目で与えられる N 個の実数の入力 (paizaランク D 相当)
 # https://paiza.jp/works/mondai/stdin_primer/stdin_primer__real_number_step2
 
-# 入出力例
-INPUT1 = <<~"EOS"
-  5
-  0.813 8.13 8 1.83 3.81
-EOS
-OUTPUT1 = <<~"EOS"
-  0.813
-  8.13
-  8
-  1.83
-  3.81
-EOS
-
 # 解答例
 # 1 行目入力
-n = gets.to_i
+n = int(input())
 # 2 行目入力
-ary = gets.split.map(&:to_f)
+ary = map(float, input().split())
 # 出力
-puts(ary)
+[print(f) for f in ary]
